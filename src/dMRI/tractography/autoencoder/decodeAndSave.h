de3c5eac050e2d82e2a73c81c7174d52ad9e8878
Kalle Saarinen <82292669+Kaapeli@users.noreply.github.com>
2025-12-16 14:24:06 +0200

    autoencoder support

---
+++ src/dMRI/tractography/autoencoder/decodeAndSave.h
@@ -0,0 +1,4 @@
+#include "StreamlineAutoencoder.h"
+using namespace NIBR;
+
+bool decodeAndSave(std::string inp, std::string out, bool force, StreamlineAutoencoder& model, int batchSize);
---
+++ src/dMRI/tractography/autoencoder/utils.h
@@ -0,0 +1,39 @@
+#pragma once
+#include <Eigen/Dense>
+#include <torch/script.h>
+#include "StreamlineAutoencoder.h"
+
+// inp:   path to input file containing latent representations of streamlines
+// out:   path to output tractogram file to save
+// force: if true, overwrites out if it already exists
+bool decodeAndSave(std::string inp, std::string out, bool force, StreamlineAutoencoder& model, int batchSize);
+
+// inp:   path to input tractogram file
+// out:   path to output file containing latent representations of streamlines
+// force: if true, overwrites out if it already exists
+bool encodeAndSave(std::string inp, std::string out, bool force, StreamlineAutoencoder& model, int batchSize);
+
+
+
+// Used for KD-tree for clustering and scoring 
+struct PointCloud {
+    std::vector<Eigen::VectorXf> points;
+
+    // Must return the number of data points
+    inline size_t kdtree_get_point_count() const { return points.size(); }
+
+    // Returns the distance between the vector "p1[0:size-1]" and the data point with index "idx_p2" stored in the class
+    inline float kdtree_distance(const float* p1, const size_t idx_p2, size_t size) const {
+        Eigen::VectorXf vec(size);
+        for (size_t i = 0; i < size; ++i)
+            vec(i) = p1[i];
+        return (vec - points[idx_p2]).squaredNorm();
+    }
+
+    // Returns the dim'th component of the idx'th point in the class
+    inline float kdtree_get_pt(const size_t idx, int dim) const { return points[idx](dim); }
+
+    // Optional bounding-box computation
+    template <class BBOX>
+    bool kdtree_get_bbox(BBOX&) const { return false; }
+};
